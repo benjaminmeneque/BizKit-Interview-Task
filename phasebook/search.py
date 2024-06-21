7fb02204df95c94f72ed9280ab8c14b3fa49989f
Benjamin <benjamin.cornejo.meneque@gmail.com>
2024-06-21 13:55:59 +0800

    implement search algorithm

--- phasebook/search.py
+++ phasebook/search.py
@@ -2,7 +2,6 @@ from flask import Blueprint, request
 
 from .data.search_data import USERS
 
-
 bp = Blueprint("search", __name__, url_prefix="/search")
 
 
@@ -27,4 +26,34 @@ def search_users(args):
 
     # Implement search here!
 
-    return USERS
+    results = []
+
+    if "id" in args:
+        user_id = args["id"]
+        user = next((user for user in USERS if user["id"] == user_id), None)
+        if user:
+            results.append(user)
+
+    if "name" in args:
+        name_query = args["name"].lower()  # Case insensitive
+        filtered_users = [user for user in USERS if name_query in user["name"].lower()]
+        results.extend(filtered_users)
+
+    if "age" in args:
+        age = int(args["age"])
+        filtered_users = [
+            user for user in USERS if user["age"] >= age - 1 and user["age"] <= age + 1
+        ]
+        results.extend(filtered_users)
+
+    if "occupation" in args:
+        occupation_query = args["occupation"].lower()  # Case insensitive
+        filtered_users = [
+            user for user in USERS if occupation_query in user["occupation"].lower()
+        ]
+        results.extend(filtered_users)
+
+    # Remove duplicates if any
+    results = list({user["id"]: user for user in results}.values())
+
+    return results
